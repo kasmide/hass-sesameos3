6ae1c0ba8291ca3515bde2f975094dbe2f83d0c5
kasmide <email@ksmd.dev>
2025-06-02 15:11:57 +0900

    use real supported platforms list when unloading entries

--- __init__.py
+++ __init__.py
@@ -1,13 +1,10 @@
 """The SesameOS 3 integration."""
 
 from __future__ import annotations
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .devices import Sesame5
 from .models import SesameConfigEntry
-
-_PLATFORMS: list[Platform] = [Platform.LOCK]
 
 async def async_setup_entry(hass: HomeAssistant, entry: SesameConfigEntry) -> bool:
     """Set up SesameOS 3 from a config entry."""
@@ -19,6 +16,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: SesameConfigEntry) -> bo
 
 async def async_unload_entry(hass: HomeAssistant, entry: SesameConfigEntry) -> bool:
     """Unload a config entry."""
-    if (unload_ok := await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)):
+    if (unload_ok := await hass.config_entries.async_unload_platforms(entry, entry.runtime_data.offers)):
         await entry.runtime_data.disconnect()
     return unload_ok
